41858c5c547df3f87a4bd191fa01ed863d4a2d90
Ilg0rbek <ilgorbekabdumalikov331@gmail.com>
2023-11-08 16:50:44 +0500

    creating rialtor project builded header forntentd

---
+++ client/src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import About from "./pages/about";
+import Profile from "./pages/profile";
+import Header from "./components/Header";
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<h1>Page not found 404 🥶😫</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
